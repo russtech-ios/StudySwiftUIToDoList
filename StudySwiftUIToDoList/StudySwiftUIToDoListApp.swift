84904816a50047320a5a69aa17831191561e72a4
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-04-07 18:52:23 +0900

    Initial Commit

---
+++ StudySwiftUIToDoList/StudySwiftUIToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIToDoListApp.swift
+//  StudySwiftUIToDoList
+//
+//  Created by Nobuki Mitsuoka on 2025/04/07.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudySwiftUIToDoListTests/StudySwiftUIToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIToDoListTests.swift
+//  StudySwiftUIToDoListTests
+//
+//  Created by Nobuki Mitsuoka on 2025/04/07.
+//
+
+import Testing
+@testable import StudySwiftUIToDoList
+
+struct StudySwiftUIToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
