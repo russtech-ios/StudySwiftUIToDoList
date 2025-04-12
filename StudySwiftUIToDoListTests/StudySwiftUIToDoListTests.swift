ca59af2dbe296a1dc92feb6d5c988c53a05f02e7
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-04-13 04:12:10 +0900

    SwiftFormat を導入

--- StudySwiftUIToDoListTests/StudySwiftUIToDoListTests.swift
+++ StudySwiftUIToDoListTests/StudySwiftUIToDoListTests.swift
@@ -5,13 +5,11 @@
 //  Created by Nobuki Mitsuoka on 2025/04/07.
 //
 
-import Testing
 @testable import StudySwiftUIToDoList
+import Testing
 
 struct StudySwiftUIToDoListTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
